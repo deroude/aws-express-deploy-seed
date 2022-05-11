9aeb4fbb21e4e2384b8e4206a0cc96e6669e3c9f
deroude <rvgabriel@yahoo.com>
2022-05-11 22:47:57 +0300

    added instance profile

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,4 +34,10 @@ resource "aws_elastic_beanstalk_environment" "environment" {
   name                = "demo-web-env"
   application         = aws_elastic_beanstalk_application.webapp.name
   solution_stack_name = "64bit Amazon Linux 2 v5.5.2 running Node.js 16"
+  
+  setting {
+      namespace = "aws:autoscaling:launchconfiguration"
+      name = "IamInstanceProfile"
+      value = "aws-elasticbeanstalk-ec2-role"
+  }
 }
